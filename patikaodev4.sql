c45e6c8789f49bcfbdebc078ea29e331f452b3d1
mmgldemir <55104826+mmgldemir@users.noreply.github.com>
2022-08-08 15:05:03 +0300

    Create patikaodev4.sql

---
+++ patikaodev4.sql
@@ -0,0 +1,12 @@
+--SELECT DISTINCT replacement_cost FROM film;
+
+--SELECT COUNT (DISTINCT replacement_cost) FROM film;
+--21
+
+--SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating='G'
+--9
+
+--SELECT COUNT(*) FROM country WHERE country LIKE '_____'
+
+--SELECT COUNT(*) from city WHERE city ILIKE '%R'; 
+--33
